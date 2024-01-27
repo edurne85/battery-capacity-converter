d4a47adc56c324b544311d94d027887fa004e9bc
Edurne <herenvardo@gmail.com>
2024-01-27 19:24:45 +0100

    chore: setup React

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <>
+            <h1>💖 Hello World!</h1>
+            <p>Welcome to your Electron + React application.</p>
+        </>
+    );
+};
+
+export { App };
