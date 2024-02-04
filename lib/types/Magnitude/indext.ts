f80f0d9af8b38a7fad3a6dd53732660f8324582b
Edurne <herenvardo@gmail.com>
2024-02-05 00:18:36 +0100

    feat(#1): unit data model for Current

---
+++ lib/types/Magnitude/indext.ts
@@ -0,0 +1,6 @@
+export type { ScalarBuilder } from './Scalar';
+export { BaseScalar, Scalar } from './Scalar';
+export type { Conversion } from './Conversion';
+
+export { CurrentUnit, Current } from './Current';
+export { TimeUnit, Time } from './Time';
